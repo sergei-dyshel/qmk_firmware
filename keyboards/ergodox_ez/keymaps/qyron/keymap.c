7a29baf9fb69047b46444275e1d3cc2799b75b05
Sergei Dyshel <qyron.private@gmail.com>
2017-12-20 18:57:27 +0200

    Swap keys according to ctrl_alt_win_laptop

--- keyboards/ergodox_ez/keymaps/qyron/keymap.c
+++ keyboards/ergodox_ez/keymaps/qyron/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_LBRC,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_EQL,
-        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MINS,
-        KC_TRNS,  KC_LSFT,  KC_LGUI,  KC_LALT,  KC_LCTL,
+        KC_TRNS,  KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,
 
                                                           KC_DEL,   KC_VOLU,
                                                                     KC_VOLD,
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGUP,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_QUOT,
                   KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_BSPC,
         KC_PGDN,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                            KC_RCTL,  KC_LALT,  KC_RGUI,  KC_RSFT,  KC_TRNS,
+                            KC_LALT,  KC_RCTL,  KC_RGUI,  KC_RSFT,  KC_TRNS,
 
         KC_MPLY,  KC_INS,
         KC_MUTE,
-        KC_CAPS,  MO(1),    KC_SPC
+        KC_ESC,   MO(1),    KC_SPC
     ),
 
 [1] = KEYMAP(
